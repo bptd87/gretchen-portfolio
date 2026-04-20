c53400f9a0b560de9f2c4ea8525ddbb59deccd3f
Brandon PT Davis <Brandon@brandonptdavis.com>
2026-04-20 00:42:04 -0700

    Update about page bio

--- client/src/content/pages/about.ts
+++ client/src/content/pages/about.ts
@@ -19,11 +19,11 @@ export const aboutPageContent = {
     },
   },
   biography: [
-    "Hey there! I'm Gretchen Ugalde, a scenic designer with roots in both Cebu City, Philippines, and the Bay Area.",
-    "Growing up, I bounced between both worlds every few years, but what remained constant was that I found my footing again through the moments I would watch movies with my father, consume art, and talk to locals about life experiences. These experiences taught me the universal power of storytelling.",
-    "My passion for design took off in college, where I immersed myself in theatre. Since then, I've continued working on my craft with Okoboji Summer Theatre, Maples Repertory Theatre, and Stephens College in central Missouri.",
-    "Currently, I'm pursuing my MFA at UC Irvine. My goal for the future is to tell many stories across the country and beyond. Most importantly, stories that my granny will love! The good thing is that she loves every story she experiences!",
-    "When I'm not designing, you can catch me hunting for trinkets at HomeGoods or TJ Maxx!",
+    "Hey there! My name is Gretchen Diane Ugalde, and I am a scenic designer based in Southern California. I hold a Masters degree in Scenic Design from The University of California, Irvine.",
+    "I am from Cebu City, Philippines, and the Bay Area! My whole life has been spent bouncing back and forth between both places--between homes, schools, and cultures. There was a lot of inconsistency growing up, but what remained constant was art: always something to make or study (I found particular comfort in Vincent Van Gogh, Fernando Amorsolo and Claude Monet), always new/old movies my dad introduced me to, always love in the different homes I lived in, and always new people to meet. I believe these experiences shaped my deep passion for the journeys of people and the universal power of storytelling.",
+    "I found theatre through my passion for the sacred, collaborative process of bringing stories to life for people to experience. Scenic design found me through my love of architecture, painting, sculpture, photography, space--and the relationship people have to place.",
+    "I am beyond grateful to create art, tell stories, and work with incredible people for a living. Looking ahead, I hope to continue telling stories across the country and beyond. I'm especially drawn to projects that amplify voices shaped by immigration and celebrate the diversity of American stories. Most importantly, I want to tell stories my granny will love--and thankfully, she loves every story she experiences!",
+    "When I'm not designing, I am hunting for trinkets at HomeGoods or TJ Maxx.",
   ],
   contact: {
     email: "gretch.ugalde@gmail.com",
